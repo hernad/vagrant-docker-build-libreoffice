80ff2e5b92bb578524c5c019d69b56ff5ecda124
Ernad Husremovic <hernad@bring.out.ba>
2015-01-02 14:50:12 +0100

    get/put u shell skripte

---
+++ lo/put_build_to_file_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SSH_OPTS="-i /root/hernad_ssh.key -o StrictHostKeyChecking=no"
+ 
+scp $SSH_OPTS LO_Platform*.zip root@files.bring.out.ba:/mnt/HD/HD_a2/bringout/Platform/linux32
+ssh $SSH_OPTS root@files.bring.out.ba chown hernad /mnt/HD/HD_a2/bringout/Platform/linux32/LO_Platform*.zip
+ 
